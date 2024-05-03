f05d657e874fb24b70d9463a0a66be617ce0ce48
lukeskw <lucasporfirioa@gmail.com>
2024-05-03 17:11:26 -0300

    feat: adding some colors to tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          850: '#112131',
+          925: '#071422',
+        },
+      },
+      fontFamily: {
+        sans: ['"Nunito"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
